5344fb6901d326dcdee44642acff888b502fd1ff
anshvorks <anshvorks1906@gmail.com>
2026-09-15 16:02:58 +0530

    add dependency injection for database sessions

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from models import Product
 from database import SessionLocal, engine
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -11,18 +12,20 @@ database_models.Base.metadata.create_all(bind=engine)
 def greet():
     return "Welcome to Vorks Track"
 
-@app.get("/products")
-def get_all_products():
-    db = SessionLocal()
-    db.query()
-    return products
-
 products = [
     Product(id=1, name="Phone", description="A smartphone", price=699.99, quantity=50),
     Product(id=2, name="Laptop", description="A powerful laptop", price=999.99, quantity=30),
     Product(id=5, name="Pen", description="A blue ink pen", price=1.99, quantity=100),
     Product(id=6, name="Table", description="A wooden table", price=199.99, quantity=20),
 ]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 def init_db():
     db = SessionLocal()
@@ -35,14 +38,18 @@ def init_db():
 
 init_db()
 
+@app.get("/products")
+def get_all_products(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Product).all()
+    return db_products
+
 # get product by its id
 
 @app.get("/product/{id}")
-def get_product_by_id(id: int):
-    for product in products:
-        if product.id == id:
-            return product
-
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
     return "product not found"
 
 # adding product
